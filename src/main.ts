7ef055a9b4492a4e478376ae8a44e092db706e65
Can Çevik <mazlumcancevik@gmail.com>
2021-08-23 14:45:36 +0300

    feat: Add swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { NestExpressApplication } from '@nestjs/platform-express'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
@@ -9,6 +10,25 @@ async function bootstrap() {
   })
 
   const configService = app.get<ConfigService>(ConfigService)
+
+  const globalPrefix = configService.get('globalPrefix')
+
+  app.setGlobalPrefix(globalPrefix)
+
+  const appName = configService.get('appName')
+  const appDescription = configService.get('appDescription')
+  const apiVersion = configService.get('apiVersion')
+
+  const config = new DocumentBuilder()
+    .setTitle(appName)
+    .setDescription(appDescription)
+    .setVersion(apiVersion)
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+
+  SwaggerModule.setup('/', app, document)
+  SwaggerModule.setup(globalPrefix, app, document)
 
   const port = configService.get('port')
   await app.listen(port)
--- src/modules/common/infrastructure/configurations/index.config.ts
+++ src/modules/common/infrastructure/configurations/index.config.ts
@@ -1,3 +1,7 @@
 export const config = () => ({
-  port: process.env.PORT || 3001
+  port: process.env.PORT || 3001,
+  globalPrefix: process.env.GLOBAL_PREFIX || 'api',
+  appName: process.env.APP_NAME,
+  appDescription: process.env.APP_DESCRIPTION,
+  apiVersion: process.env.API_VERSION
 })
