658bad9e9c1cea3691f8dbbf40d4b6cf628c9c19
Can Çevik <mazlumcancevik@gmail.com>
2023-10-27 13:53:06 +0300

    refactor: refactor response mapping interceptor

--- src/features/common/infrastructure/rest/interceptors/response-mapping.interceptor.ts
+++ src/features/common/infrastructure/rest/interceptors/response-mapping.interceptor.ts
@@ -7,24 +7,35 @@ import {
 import { Observable } from 'rxjs'
 import { map } from 'rxjs/operators'
 import { Reflector } from '@nestjs/core'
+import { Response } from 'express'
+import { Message } from '@common/infrastructure/decorators/message.decorator'
 
-export interface Response<T> {
+export interface Payload<T> {
   payload: T
+  statusCode: number
+  message: string
 }
 
 @Injectable()
 export class ResponseMappingInterceptor<T>
-  implements NestInterceptor<T, Response<T>>
+  implements NestInterceptor<T, Payload<T>>
 {
   constructor(private readonly reflector: Reflector) {}
 
   intercept(
     context: ExecutionContext,
     next: CallHandler
-  ): Observable<Response<T>> {
-    const target = context.getHandler()
-    const message = this.reflector.get('message', target)
+  ): Observable<Payload<T>> {
+    return next
+      .handle()
+      .pipe(map((payload) => this.transformResponse(payload, context)))
+  }
 
-    return next.handle().pipe(map((payload) => ({ message, payload })))
+  transformResponse<T>(payload: T, context: ExecutionContext): Payload<T> {
+    const handler = context.getHandler()
+    const message = this.reflector.get(Message, handler)
+    const { statusCode } = context.switchToHttp().getResponse<Response>()
+
+    return { message, statusCode, payload }
   }
 }
